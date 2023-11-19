48099f5a7db41e2ab12826738799bd41a3822dc0
erdenetugsganbaatar <erdenetugsganbaaatar@gmail.com>
2023-11-19 14:53:44 +0800

    added pointer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup>
-import Visual from '../components/Visual.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+
+import sleep from '../utils/sleep';
+import { computeElementHeight } from '../utils/element';
 
 const array = ref([
   {
@@ -35,28 +37,96 @@ const array = ref([
   },
 ]);
 
-const sortArray = () => {
-  const j = 1;
-  let temp = array.value[j].order;
-  array.value[j].order = array.value[j + 1].order;
-  array.value[j + 1].order = temp;
-  //   for (let i = 0; i < array.value.length; i++) {
-  //     for (let j = 0; j < array.value.length - i - 1; j++) {
-  //       if (array.value[j] > array.value[j + 1]) {
-  //         let temp = array.value[j];
-  //         array.value[j] = array.value[j + 1];
-  //         array.value[j + 1] = temp;
-  //       }
-  //     }
-  //   }
+const i = ref(0);
+const j = ref(0);
+
+const elementWidth = computed(() => {
+  // 600 is width of container
+  return 600 / array.value.length;
+});
+const arrayWithHeight = computed(() => {
+  const arrayValues = array.value.map(el => el.value);
+  const min = Math.min(...arrayValues);
+  const max = Math.max(...arrayValues);
+  return array.value.map(el => ({
+    ...el,
+    height: computeElementHeight({ min, max, value: el.value }),
+    style: {
+      width: `${elementWidth.value}px`,
+      left: `${elementWidth.value * el.order}px`,
+    },
+  }));
+});
+
+const sortArray = async () => {
+  const tempArray = [...array.value];
+  for (i.value = 0; i.value < tempArray.length; i.value++) {
+    for (j.value = 0; j.value < tempArray.length - i.value - 1; j.value++) {
+      if (tempArray[j.value].value > tempArray[j.value + 1].value) {
+        const tempOrder = tempArray[j.value].order;
+        tempArray[j.value].order = tempArray[j.value + 1].order;
+        tempArray[j.value + 1].order = tempOrder;
+        const tempVal = tempArray[j.value];
+        tempArray[j.value] = tempArray[j.value + 1];
+        tempArray[j.value + 1] = tempVal;
+      }
+      await sleep(1000);
+    }
+  }
 };
 </script>
 
 <template>
   <main>
-    <Visual :array="array" />
+    <div class="container">
+      <div class="element" v-for="el in arrayWithHeight" :style="el.style" :key="el.key">
+        <div class="column" :style="`height: ${el.height}%`"></div>
+        <span class="number">{{ el.value }}</span>
+      </div>
+      <div>
+        <div v-for="pointerIndex in [i, j]" class="pointer" :style="`left: ${elementWidth * Math.min(pointerIndex, array.length - 1) + elementWidth / 2}px`">
+          {{ pointerIndex }}
+        </div>
+      </div>
+    </div>
     <button @click="sortArray">auto sort array</button>
   </main>
 </template>
 
-<style></style>
+<style scoped>
+.container {
+  width: 600px;
+  height: 200px;
+  margin-bottom: 50px;
+  display: flex;
+  background-color: aliceblue;
+  position: relative;
+  .element {
+    position: absolute;
+    transition: all 1s ease;
+    height: 100%;
+    left: 0;
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+    .column {
+      background-color: green;
+    }
+    .number {
+      color: white;
+      background-color: black;
+      height: 20px;
+      font-size: 20px;
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+    }
+  }
+  .pointer {
+    position: relative;
+    top: 100%;
+  }
+}
+</style>
